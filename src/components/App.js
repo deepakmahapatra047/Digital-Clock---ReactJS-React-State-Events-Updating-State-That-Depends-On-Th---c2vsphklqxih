96e0d186b1d3991168537bd94f9a3f8bc50be97f
Deepak Mahapatra <deepakmahapatra047@gmail.com>
2022-11-04 18:55:33 +0530

    y

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
 const App = () => {
 
+ const [set, setState]= useState(new Date().toLocaleString())
+ 
+  setInterval(()=>{
+    setState (new Date().toLocaleString());
+  },1000);
+
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{set}</div>
     </div>
   )
 }
